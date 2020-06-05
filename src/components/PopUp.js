26611ab944066df4b431e6193c9d9ff1887ca4bd
Jorge Ivan Guevara Perez <jorgei.guevarap@utadeo.edu.co>
2020-06-04 19:23:24 -0500

    Actualización

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -6,50 +6,51 @@ import './style_popup.css'
 export default class PopUp extends Component { 
 	
 	handleClick = () => {
-	this.props.toggle();
-	  };
+		this.props.toggle();
+	};
 	  
-  state = { 
-	// Initially, no file is selected 
-	selectedFile: null
+	state = { 
+		// Initially, no file is selected 
+		selectedFile: null
 	}; 
 	
 	// On file select (from the pop up) 
 	onFileChange = event => { 
 	
-	// Update the state 
-	this.setState({ selectedFile: event.target.files[0] });
-	console.log(event.target.files[0])
+		// Update the state 
+		this.setState({ selectedFile: event.target.files[0] });
+		console.log(event.target.files[0])
 	}; 
 	
 	// On file upload (click the upload button) 
 	onFileUpload = () => { 
 	
-	// Create an object of formData 
-    const formData = new FormData(); 
-	
-	// Update the formData object
+		// Create an object of formData 
+		const formData = new FormData(); 
+		
+		// Update the formData object
 
-	formData.append(
-		"file",
-		this.state.selectedFile) 
+		formData.append(
+			"files",
+			this.state.selectedFile) 
 
-	// Details of the uploaded file 
-	console.log(formData.get("file")); 
-	
-	// Request made to the backend api 
-	// Send formData object 
-	axios.post('http://localhost:5000/api/FileUpload', formData, 
-		{
-			headers : {
-				'Content-Type': 'multipart/form-data'
-			}
-		}
-		)
-	.then(res => { // then print response status
-	console.log(res.statusText)
-  })
-
+		// Details of the uploaded file 
+		console.log(formData.get("files")); 
+		
+		// Request made to the backend api 
+		// Send formData object 
+		axios.post('http://localhost:5000/api/FileUpload', formData, 
+			{
+				headers : {
+					'Content-Type': 'multipart/form-data'
+				}
+			})
+			.then(res => { // then print response status
+				console.log(res.statusText)
+			})
+			.catch(function(){
+				console.log('SUCCESS!!');
+			})
 	}; 
 
 	// File content to be displayed after 
@@ -83,23 +84,24 @@ export default class PopUp extends Component {
     return (
       <div className = "modal">
         <div className="modal_content">
-          <span onClick={this.handleClick}>
-            <span className="close-button topright" onChange={this.onFileChange}>X</span>
-          </span>
-		  <h1> 
-			Quantil-Ecopetrol 
-			</h1> 
-			<h3> 
-			Cargar Archivo 
-			</h3> 
-			<div> 
-				<input type="file" onChange={this.onFileChange} />
+
+			<div>
+			<span onClick={this.handleClick}>
+			<p className="close-popup topright" onChange={this.onFileChange}>X</p></span>
+			<span className = "header-popup topleft"><h3>Revisión de los datos</h3></span>
+			</div>
+
+			<div className = "body-popup">
+				<div className = "body-style"><h4>Cantidad de registros actuales: 71'000.000</h4></div> 
+				<button className = "Button-secondary">Ver</button>
 				
-				<button className="button-cargar" onClick={this.onFileUpload}> 
-				Cargar! 
-				</button>
-				
-				{this.fileData()}
+				<div> 
+					<input type="file" onChange={this.onFileChange} />
+					
+					<button className="button-cargar" onClick={this.onFileUpload} > Cargar! </button>
+					
+					{this.fileData()}
+				</div>
 			</div> 
 			 
         </div>
